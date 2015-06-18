6848fcd56cbd3ba56bd5ea6cb7ccabccf9c3fda2
lnmnd <lnmnd@users.noreply.github.com>
2015-06-18 22:55:05 +0200

    eval buffer and add keymaps

--- emacs.d/my/php.el
+++ emacs.d/my/php.el
@@ -24,3 +24,17 @@ The output will appear in the buffer *PHP*."
   (interactive "r")
   (comint-send-region php-buffer start end)
   (comint-send-string php-buffer "\n"))
+
+(defun php-eval-buffer ()
+  "Send the buffer to PHP REPL for execution.
+The output will appear in the buffer *PHP*."
+  (interactive)
+  (php-eval-region (point-min) (point-max)))
+
+(defvar php-mode-map
+  (let ((m (make-sparse-keymap)))
+    (define-key m "\C-c\C-r" 'php-eval-region)
+    (define-key m "\C-c\C-k" 'php-eval-buffer)
+    m))
+
+(use-local-map php-mode-map)
