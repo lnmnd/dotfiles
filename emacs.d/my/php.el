b58adba7977600a363ea3b2c2f06bf2b0574eefc
lnmnd <lnmnd@users.noreply.github.com>
2015-06-18 22:23:36 +0200

    eval region

--- emacs.d/my/php.el
+++ emacs.d/my/php.el
@@ -8,7 +8,19 @@
 (add-hook 'php-mode-hook 'php-enable-symfony2-coding-style)
 ;; Enable flycheck
 (add-hook 'php-mode-hook 'flycheck-mode)
+
 ;; Use psysh as REPL
+
+(setq php-buffer nil)
+
 (defun run-php ()
   (interactive)
-  (pop-to-buffer (make-comint "php" "psysh")))
+  (setq php-buffer (make-comint "PHP" "psysh"))
+  (pop-to-buffer php-buffer))
+
+(defun php-eval-region (start end)
+  "Send the region between `start' and `end' to PHP REPL for execution.
+The output will appear in the buffer *PHP*."
+  (interactive "r")
+  (comint-send-region php-buffer start end)
+  (comint-send-string php-buffer "\n"))
