0fe0677398ef557a78963eea00ff6338c14df105
lnmnd <lnmnd@users.noreply.github.com>
2015-06-18 21:51:36 +0200

    run psysh as php repl

--- emacs.d/my/php.el
+++ emacs.d/my/php.el
@@ -1,3 +1,5 @@
+(require 'comint)
+
 ;; Enable php-mode
 (add-to-list 'auto-mode-alist '("\\.php\\'" . php-mode))
 ;; Use web-mode with templates
@@ -6,3 +8,7 @@
 (add-hook 'php-mode-hook 'php-enable-symfony2-coding-style)
 ;; Enable flycheck
 (add-hook 'php-mode-hook 'flycheck-mode)
+;; Use psysh as REPL
+(defun run-php ()
+  (interactive)
+  (pop-to-buffer (make-comint "php" "psysh")))
