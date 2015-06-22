6dcd93cfcbe0567435118f5e10a0a6adc3f8090c
lnmnd <lnmnd@users.noreply.github.com>
2015-06-22 22:37:38 +0200

    use-package: factor+fuel

--- emacs.d/my/factor.el
+++ emacs.d/my/factor.el
@@ -1,5 +1,9 @@
 ;; factor
-(add-to-list 'load-path "~/src/factor/misc/fuel")
-(require 'factor-mode)
-(require 'fuel-mode)
-(setq fuel-factor-root-dir "~/src/factor")
+(use-package
+  factor-mode
+  :load-path "~/src/factor/misc/fuel"
+  :config
+  (use-package
+    fuel-mode
+    :init
+    (setq fuel-factor-root-dir "~/src/factor")))
