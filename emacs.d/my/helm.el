1d9825138854eb0f7d36a4d8b4453b2036385e04
lnmnd <lnmnd@users.noreply.github.com>
2015-06-22 22:19:36 +0200

    use-package: helm

--- emacs.d/my/helm.el
+++ emacs.d/my/helm.el
@@ -1,24 +1,32 @@
 ;; helm
-(require 'helm)
-(require 'helm-config)
 
-(helm-mode 1)
+(use-package
+  helm
+  :ensure t
+  :config
+  (use-package helm-config)
+  (helm-mode 1)
+  
+  (setq helm-buffers-fuzzy-matching t
+	helm-recentf-fuzzy-match    t)
+  
+  (global-set-key (kbd "C-x C-f") 'helm-find-files)
+  (global-set-key (kbd "M-x") 'helm-M-x)
+  (global-set-key (kbd "C-x b") 'helm-mini)
 
-(setq helm-buffers-fuzzy-matching t
-      helm-recentf-fuzzy-match    t)
-
-(global-set-key (kbd "C-x C-f") 'helm-find-files)
-(global-set-key (kbd "M-x") 'helm-M-x)
-(global-set-key (kbd "C-x b") 'helm-mini)
-
-;; From within a helm-find-files search a file/directory with C-s
-(when (executable-find "ack-grep")
-  (setq helm-grep-default-command "ack-grep -Hn --no-group --no-color %e %p %f"
-        helm-grep-default-recurse-command "ack-grep -H --no-group --no-color %e %p %f"))
+  ;; From within a helm-find-files search a file/directory with C-s
+  (when (executable-find "ack-grep")
+    (setq helm-grep-default-command "ack-grep -Hn --no-group --no-color %e %p %f"
+	  helm-grep-default-recurse-command "ack-grep -H --no-group --no-color %e %p %f")))
 
 
 ;; helm-projectile
-(projectile-global-mode)
-(setq projectile-enable-caching t)
-(setq projectile-completion-system 'helm)
-(helm-projectile-on)
+(use-package
+  helm-projectile
+  :ensure t
+  :config
+  (projectile-global-mode)
+  (setq projectile-enable-caching t)
+  (setq projectile-completion-system 'helm)
+  (helm-projectile-on))
+
