49332ebfe2d8ed0bbe4742c4d7ba760fb093bf77
lnmnd <lnmnd@users.noreply.github.com>
2015-07-01 19:53:34 +0200

    use-package: php

--- emacs.d/my/php.el
+++ emacs.d/my/php.el
@@ -1,67 +1,69 @@
 (require 'comint)
 
 ;; Enable php-mode
-(add-to-list 'auto-mode-alist '("\\.php\\'" . php-mode))
-;; Use web-mode with templates
-(setq php-template-compatibility nil)
-;; Enable Symfony coding style
-(add-hook 'php-mode-hook 'php-enable-symfony2-coding-style)
-;; Enable flycheck
-(add-hook 'php-mode-hook 'flycheck-mode)
-(setq flycheck-phpmd-rulesets '("cleancode" "codesize" "controversial" "design" "unusedcode"))
-(setq flycheck-phpcs-standard "PSR2")
+(use-package
+  php-mode
+  :ensure t
+  :bind
+  ("C-c C-z" . run-php)
+  ("C-c C-r" . php-eval-region)
+  ("C-c C-k" . php-eval-buffer)
+  ("C-c C-d" . php-display-doc)
+  ("C-c C-s" . php-show-source)
+  :init
+  ;; Use psysh as REPL
+  (setq inferior-php-command "psysh")
+  (setq php-buffer nil)
 
-;; Use psysh as REPL
+  (defun run-php ()
+    (interactive)
+    (let ((cmds (split-string inferior-php-command)))
+      (setq php-buffer (apply 'make-comint "PHP" (car cmds)
+			      nil (cdr cmds))))
+    (pop-to-buffer php-buffer))
 
-(setq inferior-php-command "psysh")
-(setq php-buffer nil)
-
-(defun run-php ()
-  (interactive)
-  (let ((cmds (split-string inferior-php-command)))
-    (setq php-buffer (apply 'make-comint "PHP" (car cmds)
-			    nil (cdr cmds))))
-  (pop-to-buffer php-buffer))
-
-(defun php-eval-region (start end)
-  "Send the region between `start' and `end' to PHP REPL for execution.
+  (defun php-eval-region (start end)
+    "Send the region between `start' and `end' to PHP REPL for execution.
 The output will appear in the buffer *PHP*."
-  (interactive "r")
-  (comint-send-region php-buffer start end)
-  (comint-send-string php-buffer "\n"))
+    (interactive "r")
+    (comint-send-region php-buffer start end)
+    (comint-send-string php-buffer "\n"))
 
-(defun php-send-to-buffer (content)
-  (comint-send-string php-buffer content)
-  (comint-send-string php-buffer "\n"))
+  (defun php-send-to-buffer (content)
+    (comint-send-string php-buffer content)
+    (comint-send-string php-buffer "\n"))
 
-(defun php-eval-buffer ()
-  "Send the buffer to PHP REPL for execution.
+  (defun php-eval-buffer ()
+    "Send the buffer to PHP REPL for execution.
 The output will appear in the buffer *PHP*."
-  (interactive)
-  ;; Ignore '<?php' at the beginning of the buffer.
-  (let* ((code (buffer-substring (point-min) (point-max)))
-	 (cleaned-code (if (string-prefix-p "<?php" code t)
-			   (substring code 5)
-			 code)))
-    (php-send-to-buffer cleaned-code)))
+    (interactive)
+    ;; Ignore '<?php' at the beginning of the buffer.
+    (let* ((code (buffer-substring (point-min) (point-max)))
+	   (cleaned-code (if (string-prefix-p "<?php" code t)
+			     (substring code 5)
+			   code)))
+      (php-send-to-buffer cleaned-code)))
 
-(defun php-display-doc ()
-  (interactive)
-  (let ((cmd (concat "doc " (thing-at-point 'symbol))))
-    (php-send-to-buffer cmd)))
+  (defun php-display-doc ()
+    (interactive)
+    (let ((cmd (concat "doc " (thing-at-point 'symbol))))
+      (php-send-to-buffer cmd)))
 
-(defun php-show-source ()
-  (interactive)
-  (let ((cmd (concat "show " (thing-at-point 'symbol))))
-    (php-send-to-buffer cmd)))
+  (defun php-show-source ()
+    (interactive)
+    (let ((cmd (concat "show " (thing-at-point 'symbol))))
+      (php-send-to-buffer cmd)))
+  :config
+  (add-to-list 'auto-mode-alist '("\\.php\\'" . php-mode))
+  ;; Use web-mode with templates
+  (setq php-template-compatibility nil)
+  ;; Enable Symfony coding style
+  (add-hook 'php-mode-hook 'php-enable-symfony2-coding-style))
 
-(defvar php-mode-map
-  (let ((m (make-sparse-keymap)))
-    (define-key m "\C-c\C-z" 'run-php)
-    (define-key m "\C-c\C-r" 'php-eval-region)
-    (define-key m "\C-c\C-k" 'php-eval-buffer)
-    (define-key m "\C-c\C-d" 'php-display-doc)
-    (define-key m "\C-c\C-s" 'php-show-source)
-    m))
-
-(use-local-map php-mode-map)
+(use-package
+  flycheck
+  :ensure t
+  :config
+  (add-hook 'php-mode-hook 'flycheck-mode)
+  (setq flycheck-phpmd-rulesets '("cleancode" "codesize" "controversial" "design" "unusedcode"))
+  (setq flycheck-phpcs-standard "PSR2"))
