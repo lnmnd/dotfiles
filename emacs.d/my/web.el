9ace317a06120537caaae01bbc882c30ef1ebdac
lnmnd <lnmnd@users.noreply.github.com>
2015-07-01 20:05:59 +0200

    web

--- emacs.d/my/web.el
+++ emacs.d/my/web.el
@@ -1,22 +1,27 @@
-;; js2-mode
-(add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
-(setq inferior-js-program-command "phantomjs")
-
 ;; tern
 (add-to-list 'load-path "~/.emacs.d/lib/tern")
 (autoload 'tern-mode "~/.emacs.d/lib/tern/emacs/tern.el" nil t)
-(add-hook 'js2-mode-hook (lambda () (tern-mode t)))
 
-;; web-mode
-(add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.twig\\'" . web-mode))
-;; no tabs
-(setq-default indent-tabs-mode nil)
-;; 2 spaces
-(setq web-mode-markup-indent-offset 2)
-(setq web-mode-ac-sources-alist
-      '(("css" . (ac-source-words-in-buffer ac-source-css-property))
-        ("html" . (ac-source-words-in-buffer ac-source-abbrev))))
+(use-package
+  js2-mode
+  :ensure t
+  :mode "\\.js\\'"
+  :config
+  (setq inferior-js-program-command "phantomjs")
+  (add-hook 'js2-mode-hook (lambda () (tern-mode t))))
 
-;; mustatche
-(add-to-list 'auto-mode-alist '("\\.mustache\\'" . mustache-mode))
+(use-package
+  web-mode
+  :ensure t
+  :mode ("\\.html?\\'" "\\.twig\\'")
+  :config
+  ;; no tabs
+  (setq-default indent-tabs-mode nil)
+  ;; 2 spaces
+  (setq web-mode-markup-indent-offset 2))
+
+(use-package
+  mustache
+  :ensure t
+  :mode "\\.mustache\\'")
+
