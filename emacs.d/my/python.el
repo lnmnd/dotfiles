c62dbd7a457d2230e229ac5915828cd059758de1
lnmnd <lnmnd@users.noreply.github.com>
2015-07-02 22:01:56 +0200

    eval region keybind

--- emacs.d/my/python.el
+++ emacs.d/my/python.el
@@ -3,5 +3,6 @@
  :ensure t
  :config
  (elpy-enable)
- (setq elpy-rpc-backend "jedi"))
+ (setq elpy-rpc-backend "jedi")
+ (define-key elpy-mode-map (kbd "C-c C-r") 'elpy-shell-send-region-or-buffer))
 
