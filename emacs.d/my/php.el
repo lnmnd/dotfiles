c8aa5819b4072ff69f0e7559b6d58253ee1efedd
lnmnd <lnmnd@users.noreply.github.com>
2015-07-13 18:47:19 +0200

    use package: php-mode

--- emacs.d/my/php.el
+++ emacs.d/my/php.el
@@ -1,11 +1,15 @@
 (require 'comint)
 
-;; Enable php-mode
-(add-to-list 'auto-mode-alist '("\\.php\\'" . php-mode))
-;; Use web-mode with templates
-(setq php-template-compatibility nil)
-;; Enable Symfony coding style
-(add-hook 'php-mode-hook 'php-enable-symfony2-coding-style)
+(use-package
+  php-mode
+  :ensure t
+  :mode "\\.php\\'"
+  :config
+  ;; Use web-mode with templates
+  (setq php-template-compatibility nil)
+  ;; Enable Symfony coding style
+  (add-hook 'php-mode-hook 'php-enable-symfony2-coding-style))
+
 
 ;; Use psysh as REPL
 
