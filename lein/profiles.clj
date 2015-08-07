98d92d892d2e3807b2ffebe8ef88db538b9488b9
lnmnd <lnmnd@users.noreply.github.com>
2015-08-07 22:00:08 +0200

    doc tools

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -4,7 +4,9 @@
                  [im.chit/vinyasa "0.3.4"]
                  [alembic "0.3.2"]]
   :plugins [[cider/cider-nrepl "0.9.1"]
-            [lein-typed "0.3.5"]]
+            [lein-typed "0.3.5"]
+            [codox "0.8.13"]
+            [lein-marginalia "0.8.0"]]
   :injections [(require '[vinyasa.inject :as inject])
                (inject/in clojure.core
                           [clojure.tools.namespace.repl :refer [refresh]]
