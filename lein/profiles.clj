cf0c7c8b662f2f590b8c2f7eb645f7fa0247ba8f
lnmnd <lnmnd@users.noreply.github.com>
2015-08-10 16:47:27 +0200

    lein tools

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -4,6 +4,9 @@
                  [im.chit/vinyasa "0.3.4"]
                  [alembic "0.3.2"]]
   :plugins [[cider/cider-nrepl "0.9.1"]
+            [lein-cljfmt "0.3.0"] ;; formatting
+            [jonase/eastwood "0.2.1"] ;; lint
+            [lein-kibit "0.1.2"] ;; static code analyzer
             [lein-typed "0.3.5"]
             [codox "0.8.13"]
             [lein-marginalia "0.8.0"]]
