a42c62dc9575408c4b442f47580147c8701c5a0f
lnmnd <lnmnd@users.noreply.github.com>
2015-10-24 13:53:27 +0200

    catch fatal errors

--- watchr-php.rb
+++ watchr-php.rb
@@ -2,7 +2,7 @@ watch( './(.*).php' ) do |md|
   result = `./vendor/bin/phpunit`
   puts result
 
-  if result.match(/FAILURES\!/)
+  if result.match(/FAILURES\!/) or result.match(/Fatal error/)
     failures = result.scan(/failure:\n\n(.*)\n/)
     system "notify-send 'Tests failed' #{failures[0]} -t 6000"
   end
