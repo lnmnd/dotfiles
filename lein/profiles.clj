fb815cc8e8c8ddd97e377d688d58784147e18825
lnmnd <lnmnd@users.noreply.github.com>
2015-11-19 18:29:22 +0100

    move cider to repl

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -3,8 +3,7 @@
                  [org.clojure/tools.namespace "0.2.11"]
                  [im.chit/vinyasa "0.3.4"]
                  [alembic "0.3.2"]]
-  :plugins [[cider/cider-nrepl "0.9.1"]
-            [lein-cljfmt "0.3.0"] ;; formatting
+  :plugins [[lein-cljfmt "0.3.0"] ;; formatting
             [jonase/eastwood "0.2.1"] ;; lint
             [lein-kibit "0.1.2"] ;; static code analyzer
             [lein-typed "0.3.5"]
@@ -12,4 +11,7 @@
             [lein-marginalia "0.8.0"]]
   :injections [(require '[vinyasa.inject :as inject])
                (inject/in [clojure.tools.namespace.repl :refer [refresh]]
-                          [alembic.still :refer [[distill pull] lein]])]}}
+                          [alembic.still :refer [[distill pull] lein]])]}
+ 
+ :repl
+ {:plugins [[cider/cider-nrepl "0.9.1"]]}}
