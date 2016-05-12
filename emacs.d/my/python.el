8a94b4d9869812bdc76e3dfdff93671045b497c1
lnmnd <lnmnd@users.noreply.github.com>
2016-05-12 20:19:32 +0200

    Eval python statements

--- emacs.d/my/python.el
+++ emacs.d/my/python.el
@@ -4,6 +4,8 @@
  :config
  (elpy-enable)
  (setq elpy-rpc-backend "jedi")
- (define-key elpy-mode-map (kbd "C-c C-r") 'elpy-shell-send-region-or-buffer))
+ (define-key elpy-mode-map (kbd "C-c C-r") 'elpy-shell-send-region-or-buffer)
+ (define-key elpy-mode-map (kbd "C-c C-e") 'elpy-shell-send-current-statement)
+ (define-key elpy-mode-map (kbd "C-x C-e") 'elpy-shell-send-current-statement))
 
 
