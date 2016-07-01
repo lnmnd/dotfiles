870971413fc5aa94866f53e24841789ca8b94d0d
lnmnd <lnmnd@users.noreply.github.com>
2016-07-01 22:59:24 +0200

    Update dependencies

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -2,8 +2,8 @@
  {:dependencies [[leiningen #=(leiningen.core.main/leiningen-version)]
                  [org.clojure/tools.namespace "0.2.11"]
                  [im.chit/vinyasa "0.4.7"]]
-  :plugins [[lein-cljfmt "0.3.0"] ;; formatting
-            [jonase/eastwood "0.2.1"] ;; lint
+  :plugins [[lein-cljfmt "0.5.3"] ;; formatting
+            [jonase/eastwood "0.2.3"] ;; lint
             [lein-kibit "0.1.2"] ;; static code analyzer
             [codox "0.8.13"]]
   :injections [(require '[vinyasa.inject :as inject])
@@ -12,5 +12,5 @@
                           [vinyasa.maven pull])]}
  
  :repl
- {:plugins [[cider/cider-nrepl "0.10.0"]
+ {:plugins [[cider/cider-nrepl "0.12.0"]
             [refactor-nrepl "2.0.0-SNAPSHOT"]]}}
