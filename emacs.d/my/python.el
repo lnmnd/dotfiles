b0c5c3829a6b28b4d4b381515050277b0e2a97fe
lnmnd <lnmnd@users.noreply.github.com>
2016-09-30 20:38:03 +0200

    Set Django runner command

--- emacs.d/my/python.el
+++ emacs.d/my/python.el
@@ -6,7 +6,8 @@
  (setq elpy-rpc-backend "jedi")
  (define-key elpy-mode-map (kbd "C-c C-r") 'elpy-shell-send-region-or-buffer)
  (define-key elpy-mode-map (kbd "C-c C-e") 'elpy-shell-send-current-statement)
- (define-key elpy-mode-map (kbd "C-x C-e") 'elpy-shell-send-current-statement))
+ (define-key elpy-mode-map (kbd "C-x C-e") 'elpy-shell-send-current-statement)
+ (setq elpy-test-django-runner-command '("./manage.py" "test" "--noinput")))
 
 
 (defun pdb-set-trace ()
