7370a2535717f864c11daadf4544a4b1ad316f69
lnmnd <lnmnd@users.noreply.github.com>
2017-03-12 15:23:17 +0100

    Don't switch to Python buffer

--- emacs.d/my/python.el
+++ emacs.d/my/python.el
@@ -1,3 +1,34 @@
+(defun my-elpy-shell-send-current-statement ()
+  "Works like elpy-shell-send-current-statement but doesn't switch to Python shell"
+  (interactive)
+  (let ((beg (python-nav-beginning-of-statement))
+        (end (python-nav-end-of-statement)))
+    (elpy-shell-get-or-create-process)
+    (print (python-shell-send-string-no-output (buffer-substring beg end)))))
+
+(defun my-elpy-shell-send-region-or-buffer (&optional arg)
+  "Works like elpy-shell-send-region-or-buffer but doesn't switch to Python shell"
+  (interactive "P")
+  ;; Ensure process exists
+  (elpy-shell-get-or-create-process)
+  (let ((if-main-regex "^if +__name__ +== +[\"']__main__[\"'] *:")
+        (has-if-main nil))
+    (if (use-region-p)
+        (let ((region (elpy-shell--region-without-indentation
+                       (region-beginning) (region-end))))
+          (setq has-if-main (string-match if-main-regex region))
+          (when (string-match "\t" region)
+            (message "Region contained tabs, this might cause weird errors"))
+          (print (python-shell-send-string-no-output region)))
+      (save-excursion
+        (goto-char (point-min))
+        (setq has-if-main (re-search-forward if-main-regex nil t)))
+      (python-shell-send-buffer arg))
+    (when has-if-main
+      (message (concat "Removed if __main__ == '__main__' construct, "
+                       "use a prefix argument to evaluate.")))))
+
+
 (use-package
  elpy
  :ensure t
@@ -7,9 +38,9 @@
  (setq elpy-rpc-python-command "python3")
  (setq elpy-rpc-backend "jedi")
  (setq python-shell-interpreter-args "-i --simple-prompt --pprint")
- (define-key elpy-mode-map (kbd "C-c C-r") 'elpy-shell-send-region-or-buffer)
- (define-key elpy-mode-map (kbd "C-c C-e") 'elpy-shell-send-current-statement)
- (define-key elpy-mode-map (kbd "C-x C-e") 'elpy-shell-send-current-statement)
+ (define-key elpy-mode-map (kbd "C-c C-r") 'my-elpy-shell-send-region-or-buffer)
+ (define-key elpy-mode-map (kbd "C-c C-e") 'my-elpy-shell-send-current-statement)
+ (define-key elpy-mode-map (kbd "C-x C-e") 'my-elpy-shell-send-current-statement)
  (define-key elpy-mode-map (kbd "<C-return>") 'isend-send)
  (setq elpy-test-django-runner-command '("./manage.py" "test" "--noinput"))
  (setq gud-pdb-command-name "python -m pdb"))
@@ -33,8 +64,7 @@
   (when (get-buffer "boot.py")
     (switch-to-buffer "boot.py")
     (mark-whole-buffer)
-    (elpy-shell-send-region-or-buffer))
-  (switch-to-buffer "*Python*"))
+    (elpy-shell-send-region-or-buffer)))
 
 (defun reset-python ()
   (interactive)
