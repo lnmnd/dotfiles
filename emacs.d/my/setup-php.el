5f8be8da073493a69ddd2b98af3da4d816b4bca6
lnmnd <lnmnd@users.noreply.github.com>
2017-06-25 18:12:34 +0200

    Remove php refactor mode

--- emacs.d/my/setup-php.el
+++ emacs.d/my/setup-php.el
@@ -11,13 +11,6 @@
   ;; Enable Symfony coding style
   (add-hook 'php-mode-hook 'php-enable-symfony2-coding-style))
 
-;; download refactor.phar
-(use-package
-  php-refactor-mode
-  :config
-  (add-hook 'php-mode-hook 'php-refactor-mode))
-
-
 ;; Use psysh as REPL
 
 (setq inferior-php-command "psysh")
