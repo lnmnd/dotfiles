347ca5609064100983819248a43c74ed8e5aca9b
lnmnd <lnmnd@users.noreply.github.com>
2018-06-30 11:47:55 +0200

    Fix helm mode

--- emacs.d/my/setup-helm.el
+++ emacs.d/my/setup-helm.el
@@ -14,9 +14,9 @@
   ;; From within a helm-find-files search a file/directory with C-s
   (when (executable-find "ack-grep")
     (setq helm-grep-default-command "ack-grep -Hn --no-group --no-color %e %p %f"
-          helm-grep-default-recurse-command "ack-grep -H --no-group --no-color %e %p %f")))  
-  :config
-  (helm-mode 1)
+          helm-grep-default-recurse-command "ack-grep -H --no-group --no-color %e %p %f"))
+
+  (helm-mode))
 
 (use-package helm-ag)
 
