a55a65d2123407e377e40e961d50250e14e85dcb
lnmnd <lnmnd@users.noreply.github.com>
2018-09-27 18:12:03 +0200

    Add keybind to pop marker stack

--- emacs.d/my/setup-lisp.el
+++ emacs.d/my/setup-lisp.el
@@ -39,6 +39,7 @@
 
 (add-hook 'emacs-lisp-mode-hook
 	  (lambda ()
+	    (define-key emacs-lisp-mode-map "\C-c\C-o" #'xref-pop-marker-stack)
 	    (define-key emacs-lisp-mode-map "\C-c\C-b" #'eval-buffer)
 	    (define-key emacs-lisp-mode-map "\C-c\C-i" #'xref-find-definitions)
 	    (define-key emacs-lisp-mode-map "\C-c\C-p" #'eval-print-last-sexp)
