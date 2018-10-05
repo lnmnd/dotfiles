7302143267275f059621c6db7cb8e6af3356abbf
lnmnd <lnmnd@users.noreply.github.com>
2018-10-05 20:25:32 +0200

    Add scope capture

--- boot/profile.boot
+++ boot/profile.boot
@@ -1,6 +1,7 @@
 (require 'boot.repl)
 (swap! boot.repl/*default-dependencies*
-       concat '[[cider/cider-nrepl "0.18.0"]])
+       concat '[[cider/cider-nrepl "0.18.0"]
+                [vvvvalvalval/scope-capture "0.3.2"]])
 
 (swap! boot.repl/*default-middleware*
        concat '[cider.nrepl/cider-middleware])
--- lein/profiles.clj
+++ lein/profiles.clj
@@ -1,11 +1,13 @@
 {:user
  {:dependencies [[leiningen #=(leiningen.core.main/leiningen-version)]
                  [org.clojure/tools.namespace "0.2.11"]
-                 [im.chit/vinyasa "0.4.7"]]
+                 [im.chit/vinyasa "0.4.7"]
+                 [vvvvalvalval/scope-capture "0.3.2"]]
   :plugins [[jonase/eastwood "0.2.3"] ;; lint
             [lein-kibit "0.1.5"] ;; static code analyzer
 ]
   :injections [(require '[vinyasa.inject :as inject])
+               (require 'sc.api)
                (inject/in [clojure.tools.namespace.repl :refer [refresh]]
                           [vinyasa.inject :refer [inject]]
                           [vinyasa.maven pull])]}
