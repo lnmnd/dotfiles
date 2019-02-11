8d232dddb3f5946cdbc48a1f2c708cacb5026c3b
lnmnd <lnmnd@users.noreply.github.com>
2019-02-11 18:57:06 +0100

    Add emacs configuration script

---
+++ emacs.d/configure-emacs.sh
@@ -0,0 +1,10 @@
+./configure \
+    --prefix $HOME/local \
+    --without-all \
+    --with-x-toolkit=lucid \
+    --with-jpeg \
+    --with-gif \
+    --with-png \
+    --with-rsvg \
+    --with-xft \
+    --with-gnutls
