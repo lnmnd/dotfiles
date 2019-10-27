42fd549c717043a9fc4d9694360cb9eb708242f9
lnmnd <lnmnd@users.noreply.github.com>
2019-10-27 15:57:58 +0100

    Add inf-clojure

--- emacs.d/my/setup-lisp.el
+++ emacs.d/my/setup-lisp.el
@@ -127,6 +127,76 @@
   (define-key clojure-mode-map (kbd "C-c C-i") #'dumb-jump-go)
   (define-key clojure-mode-map (kbd "C-c C-o") #'dumb-jump-back))
 
+(defun clj-connect ()
+  (interactive)
+  (inf-clojure-connect "localhost" 6666))
+
+(defun clj-add-lib ()
+  (interactive)
+  (let ((coordinates (read-string "Coordinates: ")))
+    (when (get-buffer "*inf-clojure*")
+      (send-string "*inf-clojure*"
+		   (concat
+		    "(in-ns 'user)"
+		    "(require '[clojure.tools.deps.alpha.repl :refer [add-lib]])"
+		    "(add-lib " coordinates ")")))))
+
+(defun clj-start ()
+  (interactive)
+  (when (get-buffer "*inf-clojure*")
+    (comint-simple-send "*inf-clojure*" "(ns user) (start)\n")))
+
+(defun clj-stop ()
+  (interactive)
+  (when (get-buffer "*inf-clojure*")
+    (comint-simple-send "*inf-clojure*" "(ns user) (stop)\n")))
+
+(defun clj-restart ()
+  (interactive)
+  (when (get-buffer "*inf-clojure*")
+    (comint-simple-send "*inf-clojure*" "(ns user) (stop) (start)\n")))
+
+(defun clj-unmap ()
+  (interactive)
+  (when (get-buffer "*inf-clojure*")
+    (comint-simple-send "*inf-clojure*" (concat "(ns-unmap *ns* '" (symbol-name (symbol-at-point)) ")"))))
+
+(defun clj-run-tests ()
+  (interactive)
+  (when (get-buffer "*inf-clojure*")
+    (comint-simple-send "*inf-clojure*" "(require '[clojure.test :refer [run-tests]]) (run-tests)")))
+
+(defun clj-eval-print-last-sexp ()
+  (interactive)
+  (when (get-buffer "*inf-clojure*")
+    (let* ((start (save-excursion (backward-sexp) (point)))
+	   (end (point))
+	   (sexp (buffer-substring-no-properties start end))
+	   (string (concat "(require '[clojure.pprint :refer [pprint]]) (pprint " sexp ")")))
+      (comint-simple-send "*inf-clojure*" string))))
+
+(use-package
+  inf-clojure
+  :config
+  ;; Patch
+  ;; inf-clojure changes comint-input-sender from comint-simple-send to inf-clojure--send-string.
+  ;; inf-clojure--send-string sets repl type and calls comint-simple-send
+  ;; If inf-clojure-buffer does not exist (nil) inf-clojure--set-repl-type throws an error:
+  ;; "Wrong type argument: stringp, nil"
+  (defun inf-clojure--send-string (proc string)
+    "Patched. Sets repl type only when inf-clojure buffer exists"
+    (when inf-clojure-buffer
+      (inf-clojure--set-repl-type proc))
+    (comint-simple-send proc string))
+  (add-hook 'clojure-mode-hook #'inf-clojure-minor-mode)
+  (define-key inf-clojure-minor-mode-map (kbd "C-c M-c") #'clj-connect)
+  (define-key inf-clojure-minor-mode-map (kbd "C-c C-k") #'inf-clojure-eval-buffer)
+  (define-key inf-clojure-minor-mode-map (kbd "C-c C-d") #'inf-clojure-show-var-documentation)
+  (define-key inf-clojure-minor-mode-map (kbd "C-c C-u") #'clj-unmap)
+  (define-key inf-clojure-minor-mode-map (kbd "C-c C-i") #'dumb-jump-go) ;; override inf-clojure-show-ns-vars
+  (define-key inf-clojure-minor-mode-map (kbd "C-c C-t") #'clj-run-tests)
+  (define-key inf-clojure-minor-mode-map (kbd "C-c C-p") #'clj-eval-print-last-sexp))
+
 (use-package
   cider
   :config
